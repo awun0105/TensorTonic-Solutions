89c5990e6f3ffe9a5fb59fb90fadcce3da7f396f
Lâm Quang Anh Quân <86759633+awun0105@users.noreply.github.com>
2026-04-21 23:14:19 +0700

    Update solution for dot-product

--- dot-product/dot-product.py
+++ dot-product/dot-product.py
@@ -4,6 +4,12 @@ def dot_product(x, y):
     """
     Compute the dot product of two 1D arrays x and y.
     Must return a float.
+    Requirements
+    Must work for lists or NumPy arrays
+    Must return a float
+    Must be vectorized (no Python element loops)
+    Must handle 1D arrays only
+    Must raise ValueError for mismatched lengths
     """
     if len(x) != len(y):
         raise ValueError 
